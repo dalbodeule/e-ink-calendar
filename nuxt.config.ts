114f237ad4b27b436f1a2c92504e48a95b67b17f
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2023-02-16 18:37:14 +0900

    add eslint/screenshot plugin create

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,40 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-extra-semi */
+import { OutputOptions } from "rollup"
+import eslintPlugin from "vite-plugin-eslint"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  nitro: {
+    plugins: ["@/plugins/nitro_puppeteer"]
+  },
+  build: {
+    transpile: ["wait"]
+  },
+  vite: {
+    plugins: [eslintPlugin()]
+  },
+  hooks: {
+    "vite:extendConfig"(clientConfig, { isClient }) {
+      if (isClient) {
+        ;(
+          clientConfig.build!.rollupOptions!.output as OutputOptions
+        ).manualChunks = {
+          "@fortawesome/vue-fontawesome": ["@fortawesome/vue-fontawesome"],
+          "@fortawesome/fontawesome-svg-core": [
+            "@fortawesome/fontawesome-svg-core"
+          ],
+          "@fortawesome/free-solid-svg-icons": [
+            "@fortawesome/free-solid-svg-icons"
+          ],
+          "@fortawesome/free-regular-svg-icons": [
+            "@fortawesome/free-regular-svg-icons"
+          ],
+          "@fortawesome/free-brands-svg-icons": [
+            "@fortawesome/free-brands-svg-icons"
+          ]
+        }
+      }
+    }
+  }
 })
---
+++ plugins/nitro_puppeteer.ts
@@ -0,0 +1,62 @@
+import puppeteer, { Browser, Page } from "puppeteer"
+import { NitroApp } from "nitropack"
+import wait from "wait"
+
+export default defineNitroPlugin((nitroApp) => {
+  // eslint-disable-next-line @typescript-eslint/no-extra-semi
+  ;(async () => {
+    const instance = new EIBrowserInstance(nitroApp)
+
+    await instance.run()
+    await instance.destroy()
+  })()
+})
+
+class EIBrowserInstance {
+  private instance?: Browser
+  private page?: Page
+
+  private nitroApp: NitroApp
+
+  private url = "https://www.naver.com/"
+
+  constructor(nitroApp: NitroApp) {
+    this.nitroApp = nitroApp
+  }
+
+  public async run() {
+    await this.launch()
+
+    await wait(1000)
+
+    await this.screenshot()
+
+    await this.destroy()
+  }
+
+  private async launch() {
+    if (this.instance) {
+      return true
+    } else {
+      this.instance = await puppeteer.launch({
+        channel: "chrome",
+        headless: true
+      })
+      this.page = await this.instance.newPage()
+
+      await this.page.goto(this.url)
+      await this.page.setViewport({ width: 1920, height: 1080 })
+    }
+  }
+
+  private async screenshot() {
+    await this.page?.screenshot({ path: "./screenshot.png" })
+    console.log(
+      `Screenshot has placed! ${this.url} ${new Date().toISOString()}`
+    )
+  }
+
+  public async destroy() {
+    await this.instance?.close()
+  }
+}
